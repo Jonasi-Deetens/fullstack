f17a4a5a8af92deb9bc4b0c431534605819b4eea
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-12-18 09:58:21 +0100

    Add database connection that returns all items from ideas table

--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
 import express from "express";
 import json_encode from "json_encode";
+import mariadb from "mariadb";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    connectionLimit: 5
+});
+
 //app.use(json_encode);
 
-app.on("listen", (req, res) => {
-    console.log("hey");
+app.get("/ideas", (req, res) => {
+    (async () => {
+        let connection;
+        try {
+            connection = await pool.getConnection();
+            const data = await connection.query(`SELECT * FROM ideas_db.ideas`);
+            console.log(data)
+        } catch(err) {
+            throw err;
+        } finally {
+            if (connection) connection.end();
+        }
+    })()
 });
 
 app.listen(process.env.PORT, () => {
